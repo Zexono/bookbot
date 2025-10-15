0dfa010b265fa49ad6b5d2154ef82bfc6948caa8
Zexono <spector6622@gmail.com>
2025-10-16 00:53:32 +0700

    finish report

--- main.py
+++ main.py
@@ -9,13 +9,25 @@ def get_book_text(filepath):
 
 
 def main():
-    txt = get_book_text("books/frankenstein.txt")
+    file_path = "books/frankenstein.txt"
+    txt = get_book_text(file_path)
     num = count_word(txt)
     chardict = count_char(txt)
-    print(f"Found {num} total words")
+    #print(f"Found {num} total words")
     charlist = dict_to_list(chardict)
     #print(charlist)
     charlist.sort(key=sort_char,reverse=True)
-    print(charlist)
+    #print(charlist)
 
+    ##report print
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at {file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num} total words")
+    print("--------- Character Count -------")
+    for char in charlist:
+        if char["char"].isalpha() == False:
+            continue
+        print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
 main()
